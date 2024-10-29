f6f059702ccf0990690ffe64f898d6f88358b325
KayWide <elkiariey@gmail.com>
2024-10-29 09:35:09 +0300

    week 1 assignment

---
+++ user_input.py
@@ -0,0 +1,8 @@
+#Request for name, age, location
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+
+#print a personalized message
+print("Hello, " + name + ". You are " + age + " years old. You live in " + location + ". Remember to drink your water.")
+
